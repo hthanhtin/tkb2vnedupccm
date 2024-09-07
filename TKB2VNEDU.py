162f6c4a62d92f89ba95c8fc1e98de747cd769ed
hthanhtin <34848730+hthanhtin@users.noreply.github.com>
2024-09-07 15:41:10 +0700

    Add files via upload

--- TKB2VNEDU.py
+++ TKB2VNEDU.py
@@ -12,26 +12,61 @@ pccm=df.to_dict(orient='records')
 def tenlop(s):
     s=s.strip()
     return loptkb2vnedu[s[:len(s)-3]]
-pc=[]
-for gv in pccm:
-    x=gv['DSLOPDAY'].split(",")
-    y=[tenlop(i) for i in x ]
-    pc.append((gv["GIAOVIEN"],f"{gv["MONVNEDU"]} : {", ".join(y)}"))
+def vnedu_pccm():
+    pc=[]
+    a=[]
+    b=[]
+    c=[]
+    for gv in pccm:
+        x=gv['DSLOPDAY'].split(",")
+        y=[tenlop(i) for i in x ]
+        a.append(gv["GIAOVIEN"])
+        b.append(gv["MONVNEDU"])
+        c.append(", ".join(y))
 
-lopsgv=dict()
-for x in pc:
-    lopsgv[x[0]]=""
+    data={
+        "GV": a,
+        "Mon":b,
+        "Lopday": c
 
-for x in pc:
-    if len(lopsgv[x[0]])==0:
-        lopsgv[x[0]]=x[1]
-    else:
-        lopsgv[x[0]]=lopsgv[x[0]]+"; "+x[1]
-a=lopsgv.keys()
-b=lopsgv.values()
-data={
-    "GV": a,
-    "Lop": b
-}
-dt=pd.DataFrame(data)
-dt.to_excel("VNEDU.xlsx")
+    }
+    try:
+        dt=pd.DataFrame(data)
+        dt.to_excel("VNEDU.xlsx")
+    except:
+        with open("vnedu_pccm.txt", "w", encoding="utf8") as f:
+            s1 = f"{len(a)} {" ".join(a)}"+"\n"
+            s2 = f"{len(b)} {" ".join(b)}" + "\n"
+            s3 = f"{len(c)} {" ".join(c)}" + "\n"
+            f.write(s1+s2+s3)
+
+def lop_ppcm():
+    pccm_lop=dict()
+    for k in loptkb2vnedu.keys():
+        pccm_lop[k]=[]
+
+    for x in pccm:
+        lops=x["DSLOPDAY"].split(",")
+        for l in lops:
+            l=l.strip()
+            l=l[0:len(l)-3]
+            pccm_lop[l].append(x["MONVNEDU"]+"-"+x["GIAOVIEN"])
+    for k in pccm_lop.keys():
+        l=pccm_lop[k]
+        l.sort()
+        pccm_lop[k]=l
+    try:
+        sv=pd.DataFrame(pccm_lop)
+        sv.to_excel("pccm_lop.xlsx")
+    except:
+        with open("log_ppcm_lop.txt","w", encoding="utf8") as f:
+            for k in pccm_lop.keys():
+                l = pccm_lop[k]
+                s=f"{len(l)};{k};{";".join(l)}"+"\n"
+                f.write(s)
+
+
+
+
+lop_ppcm()
+vnedu_pccm()
